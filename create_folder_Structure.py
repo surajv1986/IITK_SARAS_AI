adaa96006e33d46421cd8d7d0485ef89f3e7a6db
Suraj Vanjeeswaran <surajv1986@users.noreply.github.com>
2024-08-07 22:46:52 +0530

    Fix Issues with create_folder_structure.py
    Fix issues with neg images for create folder structure.py

--- create_folder_Structure.py
+++ create_folder_Structure.py
@@ -20,13 +20,30 @@ NEG_PATH = os.path.join('data', 'negative')
 ANC_PATH = os.path.join('data', 'anchor')
 
 #Make the directories
-os.makedirs(POS_PATH)
-os.makedirs(NEG_PATH)
-os.makedirs(ANC_PATH)
+#os.makedirs(POS_PATH)
+#os.makedirs(NEG_PATH)
+#os.makedirs(ANC_PATH)
+# Get the current working directory
+cwd = os.getcwd()
 
-#move lfw images to data/negative
-for directory in os.listdir('lfw'):
-    for file in os.listdir(os.path.join('lfw', directory)):
-        EX_PATH = os.path.join('lfw', directory, file)
-        NEW_PATH = os.path.join(NEG_PATH, file)
-        os.replace(EX_PATH, NEW_PATH)
+# Join the CWD with 'lfw' to get the correct path
+lfw_path = os.path.join(cwd, 'lfw')
+print(lfw_path)
+
+
+for directory in os.listdir(lfw_path):
+    dir_path = os.path.join(lfw_path, directory)
+    print(dir_path)
+    if not os.path.exists(dir_path):
+        print(f"Error: {dir_path} does not exist")
+    elif not os.path.isdir(dir_path):
+        print(f"Error: {dir_path} is not a directory")
+    else:
+        for file in os.listdir(dir_path):
+            file_path = os.path.join(dir_path, file)
+            if os.path.isfile(file_path):  # Check if it's a file
+                NEW_PATH = os.path.join(NEG_PATH, file)
+                os.replace(file_path, NEW_PATH)
+                print(f"File {file} copied to {NEG_PATH}")
+            else:
+                print('not a file')
